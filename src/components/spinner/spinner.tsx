7e3301bbd4466b8d9629d058b9c756812fa71473
waitingwittykitty <hryschiller@gmail.com>
2022-08-24 10:06:36 -0400

    feat: style spinner

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -1,9 +1,20 @@
+import './spinner.scss';
+
 interface SpinnerProps {
   visible: boolean;
 }
 
 function Spinner({ visible }: SpinnerProps) {
-  return visible ? <div>loading...</div> : null;
+  return visible ? (
+    <div className="spinner">
+      <div className="lds-ring">
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default Spinner;
