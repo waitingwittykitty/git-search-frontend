a1fc7bdc175e613deca99e1a63dcbb52f9f54f73
waitingwittykitty <hryschiller@gmail.com>
2022-08-24 08:44:28 -0400

    feat: style search form

--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -2,6 +2,8 @@ import { SyntheticEvent, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
 
+import Search from './search.svg';
+
 import './search-form.scss';
 
 function SearchForm() {
@@ -24,10 +26,17 @@ function SearchForm() {
   };
 
   return (
-    <form method="GET" action="/search" onSubmit={handleSubmit}>
-      <input type="text" name="word" value={word} onChange={handleChangeWord} />
-      <button type="submit">Search</button>
-    </form>
+    <div className="form-container">
+      <form className="search-form" method="GET" action="/search" onSubmit={handleSubmit}>
+        <div className="wrapper">
+          <span className="image-container">
+            <img src={Search} alt="search" />
+          </span>
+          <input type="text" name="word" value={word} onChange={handleChangeWord} />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
