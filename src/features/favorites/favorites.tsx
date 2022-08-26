c494e85654a785f967b6a0eb4fb93e1b1632583f
waitingwittykitty <hryschiller@gmail.com>
2022-08-26 14:44:45 -0400

    feat: add removeFromFavorites button

--- src/features/favorites/favorites.tsx
+++ src/features/favorites/favorites.tsx
@@ -1,13 +1,18 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import Button from '../../components/button/button';
+import Confirm from '../../components/confirm/confirm';
+import Modal from '../../components/modal/modal';
+import { notify } from '../../components/notification/notification-reducer';
 import Pagination from '../../components/pagination/pagination';
 import Spinner from '../../components/spinner/spinner';
 import Table from '../../components/table/table';
 import {
   fetchFavoritesAsync,
+  Fork,
+  removeFavoriteAsync,
   selectFavoritesLoading,
   selectFavoritesPageCount,
   selectFavoritesResult,
@@ -23,6 +28,8 @@ function Favorites() {
   const page = Number(searchParams.get('page')) || 1;
   const perPage = Number(searchParams.get('height')) || 10;
   const pageCount = useAppSelector(selectFavoritesPageCount(perPage));
+  const [isConfirmOpened, setIsConfirmOpened] = useState(false);
+  const [selectedFork, setSelectedFork] = useState<Fork | null>(null);
   const forks = useMemo(
     () => result?.slice((page - 1) * perPage, page * perPage),
     [result, page, perPage]
@@ -37,6 +44,23 @@ function Favorites() {
       ...Object.fromEntries(searchParams.entries()),
       page: page.toString(),
     });
+  };
+
+  const handleRemoveFavorite = (fork: Fork) => () => {
+    setIsConfirmOpened(true);
+    setSelectedFork(fork);
+  };
+
+  const handleConfirmRemoveFavorite = async () => {
+    if (selectedFork) {
+      await dispatch(removeFavoriteAsync(selectedFork.id));
+      await dispatch(fetchFavoritesAsync());
+      dispatch(notify({ title: 'Success', description: 'Removed successfully' }));
+    }
+  };
+
+  const toggleConfirm = () => {
+    setIsConfirmOpened(isConfirmOpened => !isConfirmOpened);
   };
 
   return (
@@ -57,6 +81,7 @@ function Favorites() {
             <th>Owner</th>
             <th>Stars</th>
             <th>Link</th>
+            <th>...</th>
           </tr>
         </thead>
         <tbody>
@@ -69,6 +94,11 @@ function Favorites() {
                 <td>
                   <a href={fork.link}>{fork.link}</a>
                 </td>
+                <td>
+                  <Button className="button" onClick={handleRemoveFavorite(fork)}>
+                    Remove from Favorites
+                  </Button>
+                </td>
               </tr>
             ))
           ) : (
@@ -78,6 +108,15 @@ function Favorites() {
           )}
         </tbody>
       </Table>
+
+      <Modal isOpened={isConfirmOpened} toggle={toggleConfirm}>
+        <Confirm
+          title="Remove from Favorites"
+          description={`Are you sure you want to remove this fork (${selectedFork?.owner}/${selectedFork?.name}) from your favorites?`}
+          onOk={handleConfirmRemoveFavorite}
+          toggle={toggleConfirm}
+        />
+      </Modal>
     </section>
   );
 }
